8541ebac2a78a69f8be7ae327f63e3a43d614032
Levi Castro <levicastro28@gmail.com>
2023-04-05 19:40:10 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import model.Gato;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+/*        int a = 5;
+        int b = 3;
+        System.out.println("Hello World!" + (a+b));*/
+
+
+        }
+    }
