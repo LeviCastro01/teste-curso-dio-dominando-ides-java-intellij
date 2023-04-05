e2e5a62fc2e506d27c4b0b399ba23084511c0868
Levi Castro <levicastro28@gmail.com>
2023-04-05 19:58:30 -0300

    Criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,6 +1,7 @@
 package br.com.dio;
 
 import model.Gato;
+import model.Livro;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -9,6 +10,9 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
 
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
 /*        int a = 5;
         int b = 3;
         System.out.println("Hello World!" + (a+b));*/
--- src/model/Gato.java
+++ src/model/Gato.java
@@ -63,3 +63,43 @@ public class Gato {
                 '}';
     }
 }
+
+class Livro {
+    private String nome;
+    private Integer numpaginas;
+
+    public Livro(String nome, Integer numpaginas){
+        this.nome = nome;
+        this.numpaginas = numpaginas;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpaginas() {
+        return numpaginas;
+    }
+
+    public void setNumpaginas(Integer numpaginas) {
+        this.numpaginas = numpaginas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numpaginas, livro.numpaginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numpaginas);
+    }
+}
